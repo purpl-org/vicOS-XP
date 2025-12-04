8cd176483bdc3d20a424ef039a381d243c6e6743
froggitti <85637569+froggitti@users.noreply.github.com>
2025-12-03 23:33:08 -0500

    change server links

--- animProcess/src/cozmoAnim/connectionFlow.cpp
+++ animProcess/src/cozmoAnim/connectionFlow.cpp
@@ -44,8 +44,8 @@ namespace {
 u32 _pin = 123456;
 
 const f32 kRobotNameScale = 0.7f;
-const std::string kURL = "v.pvic.xyz";
-const ColorRGBA   kColor(0.9f, 0.5f, 0.9f, 1.f);
+const std::string kURL = "v.p-2.xyz";
+const ColorRGBA   kColor(1.0f, 1.0f, 1.0f, 1.f);
 
 const char* kShowPinScreenSpriteName = "pairing_icon_key";
 
